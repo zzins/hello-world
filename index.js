d210dcc70653e8649e23afc1fffb049319c78eb9
leejinhui <admin@nyaongnyaong.com>
2021-11-29 14:16:45 +0900

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + `/public`));
+
+app.get("/hello", (request, response) => {
+    response.render("hello", {
+        name: request.query.nameQuery,
+    });
+});
+
+app.get("/hello:nameParam", (request, response) => {
+    response.render("hello", {
+        name: request.params.nameParam,
+    });
+});
+
+/*
 app.get("/", (request, response) => {
     response.send("Hello, world");
 });
+*/
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost${port}`);
